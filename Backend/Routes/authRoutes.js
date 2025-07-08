779d29e4343a7672402af9b94619b7c3a4df4071
tanish4787 <tanish4787@gmail.com>
2025-07-08 19:07:46 +0530

    lst

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -4,14 +4,13 @@ import { authMiddleware } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
 router.get("/validate-token", authMiddleware, (req, res) => {
   res.status(200).json({
     success: true,
     user: req.user,
   });
 });
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
